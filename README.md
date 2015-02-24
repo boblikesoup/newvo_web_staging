0670591baf2b024f2e575aa1c4effd7d4fe585d0
boblikesoup <johnuke@gmail.com>
2015-02-24 08:44:11 -0800

    added files untested

---
+++ README.md
@@ -0,0 +1,22 @@
+newvo_web
+=========
+
+For testing the API and mobile apps.
+
+
+
+
+Website to-do's:
+
+Landing page needs social media links - Facebook, Twitter, Instagram, Pinterest, Tumblr
+
+Navigation
+-Home
+-Explore (use app)
+-Blog (emphasize)
+-About
+
+Footer (social media links?)
+-Copyright
+-Contact
+-Privacy# newvo_web_staging
---
+++ cloud/app.js
@@ -0,0 +1,33 @@
+// To Do's:
+// Get controllers working
+// Modify routes/API
+// Set get '/' to visit cloud/views/index.jade instead of public/index.html
+
+require('controllers/main')
+
+// These two lines are required to initialize Express in Cloud Code.
+var express = require('express');
+var AppLinks = require('applinks-metatag');
+var app = express();
+
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'jade');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+
+// Renders public/index.html
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+// Renders cloud/views/index.jade
+app.get('/hello', function(req, res) {
+    res.render('index');
+});
+
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
+});
+
+// Attach the Express app to Cloud Code.
+app.listen();
